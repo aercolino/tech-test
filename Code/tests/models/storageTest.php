6a52f93c436cd184d005bada58e6aa5dfbde9efe
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-01-29 10:42:11 +0100

    added a Storage class

---
+++ Code/tests/models/storageTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once( dirname(__FILE__, 3) . '/src/config.php' );
+require_once(CONFIG::get('ABSPATH') . '/src/models/storage.php'); 
+
+class StorageTest extends PHPUnit_Framework_TestCase
+{
+    public function testWhatYouWriteIsReadBack()
+    {
+        $data = array(
+            'this' => 'is', 
+            'a' => 'sample'
+        );
+        Storage::write($data);
+        $read = Storage::read();
+        $this->assertEquals($data, $read);
+    }
+}
