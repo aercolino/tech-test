d37f01bc1e414e351a492dc29ddbf098590756cf
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2016-01-29 12:50:13 +0100

    added update functionality
    - together with listing stored people

--- Code/src/controllers/people/a_list.php
+++ Code/src/controllers/people/a_list.php
@@ -1,11 +1,48 @@
 <?php
 
+require_once CONFIG::get('ABSPATH') . '/src/models/person.php';
+require_once CONFIG::get('ABSPATH') . '/src/models/storage.php';
+
 $display['page_title'] = 'People';
 
-// $data = post_data();
-// list( $display['page_error'], $errors ) = post_errors();
+list( $display['page_error'], $errors ) = post_errors();
+if ($display['page_error']) {
+    d_($display['page_error']);
+}
+if ($errors) {
+    d_($errors);
+}
 
-// $display['button_OK'] = merge_params( $url_string, array( 'action' => 'save' ) );
+$stored_people = Storage::read() ?? [];
+if (count($stored_people)) {
+    $display['stored_people'] = array_map(function ($item) { return "$item"; }, $stored_people);
+    $display['people'] = array_map(function ($item) { return $item->toHash(); }, $stored_people);
+}
+else {
+    $display['stored_people'] = [];
+    $display['people'][] = array(
+        'firstname' => 'Jeff',
+        'surname' => 'Stelling',
+    );
+    $display['people'][] = array(
+        'firstname' => 'Chris',
+        'surname' => 'Kamara',
+    );
+    $display['people'][] = array(
+        'firstname' => 'Alex',
+        'surname' => 'Hammond',
+    );
+    $display['people'][] = array(
+        'firstname' => 'Jim',
+        'surname' => 'White',
+    );
+    $display['people'][] = array(
+        'firstname' => 'Natalie',
+        'surname' => 'Sawyer',
+    );
+}
+
+$display['button_OK'] = merge_params( $url_string, array( 'action' => 'save' ) );
 
 
 render( CONFIG::get('ABSPATH').'/src/views/templates/people/list.php', CONFIG::get('ABSPATH').'/src/views/layouts/flat.php' );
---
+++ Code/src/controllers/people/a_save.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once CONFIG::get('ABSPATH') . '/src/models/person.php';
+require_once CONFIG::get('ABSPATH') . '/src/models/storage.php';
+
+try {
+
+    $people = $_POST['people'];
+    foreach ($people as $key => $value) {
+        $people[$key] = new Person($value);  // this cleans up posted data
+    }
+
+    Storage::write($people);    
+    $_SESSION['info'] = 'People saved.';              
+    
+    redirect_to( '/index.php?controller=people&action=list' );
+}
+catch ( Exception $e ) {
+    $_SESSION['page_error'] = $e->getMessage();
+    $_SESSION['page_post'] = $_POST;
+    $_SESSION['errors'] = $errors;
+    redirect_to( '/index.php?controller=people&action=list' );
+}
